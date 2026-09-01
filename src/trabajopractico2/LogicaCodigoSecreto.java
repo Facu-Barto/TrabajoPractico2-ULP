e424777a91d5af1a352d6a276d7807ff19c2f406
facubarto05 <facubarto2005@gmail.com>
2026-09-01 20:03:14 -0300

    Cree la clase de la logica del ejercicio, y modifique algunas cosas del GUI.

--- src/trabajopractico2/LogicaCodigoSecreto.java
+++ src/trabajopractico2/LogicaCodigoSecreto.java
@@ -1,5 +1,29 @@
 package trabajopractico2;
 
 public class LogicaCodigoSecreto {
+    private int codigoSecreto;
+    private static int contadorIntentos;
+    private static int contadorAyudas;
     
+    public void generarCodigo(){
+        //genera el codigo secreto.
+    }
+    
+    public void esMayor(int intento){
+        //Retorna true o false para saber si el código es mayor o menor a lo que puso el usuario.
+    }
+    
+    public void esCorrecto(int intento){
+        //Verifica si adivinó el número
+    }
+    
+    public int revelarNumero(int posicion){
+        //Devuelve el numero de una poscion(uso del botón Revelar o cuando el usuario acierta la posición)
+        return 0;
+    }
+    
+    public int puedeUsarAyuda(){
+        //Se fija si la variable contadorAyudas es menor a 2.
+        return 0;
+    } 
 }
